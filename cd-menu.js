b8428fa74bc73dd94a9da601be909158f27ed4e4
Daniel Brierton <daniel@coderdojo.org>
2016-08-08 09:56:17 +0100

    Tidied JS, fixed z-index issue in CSS

--- cd-menu.js
+++ cd-menu.js
@@ -1,197 +1,187 @@
 /* global XMLHttpRequest */
-if (!window.cdMenuConfig) {
-  window.cdMenuConfig = {};
-}
 
 (function () {
+  if (!window.cdMenuConfig) {
+    window.cdMenuConfig = {};
+  }
+
   var zenBase = window.cdMenuConfig.zenBase || 'https://zen.coderdojo.com';
+
   function each (it, cb) {
     return Array.prototype.forEach.call(it, cb);
   }
 
+  // Sliding Menu
   window.addEventListener('load', function () {
-    // Sliding Menu
-    (function () {
-      var toggle = document.querySelector('.cd-menu__hamburger');
-      var slidingMenu = document.querySelector('#cd-menu__slidingMenuPrimary');
-      var scrim = document.querySelector('.cd-menu__scrim');
-      var secondarySlidingMenus = document.querySelectorAll('.cd-menu__slidingMenuSecondary');
-      var closeButtons = document.querySelectorAll('.cd-menu__closeButton');
-      var menuExpansions = document.querySelectorAll('.cd-menu__slidingMenu .cd-menu__content > ul > li > span');
+    var toggle = document.querySelector('.cd-menu__hamburger');
+    var slidingMenu = document.querySelector('#cd-menu__slidingMenuPrimary');
+    var scrim = document.querySelector('.cd-menu__scrim');
+    var secondarySlidingMenus = document.querySelectorAll('.cd-menu__slidingMenuSecondary');
+    var closeButtons = document.querySelectorAll('.cd-menu__closeButton');
+    var menuExpansions = document.querySelectorAll('.cd-menu__slidingMenu .cd-menu__content > ul > li > span');
 
-      function closeDrawer () {
-        slidingMenu.setAttribute('data-toggle', 'closed');
-        scrim.className = 'cd-menu__scrim';
-        each(secondarySlidingMenus, function (secondarySlidingMenu) {
-          secondarySlidingMenu.setAttribute('data-toggle', 'closed');
+    function closeDrawer () {
+      slidingMenu.setAttribute('data-toggle', 'closed');
+      scrim.className = 'cd-menu__scrim';
+      each(secondarySlidingMenus, function (secondarySlidingMenu) {
+        secondarySlidingMenu.setAttribute('data-toggle', 'closed');
+      });
+    }
+
+    function openDrawer () {
+      slidingMenu.setAttribute('data-toggle', 'open');
+      scrim.className = 'cd-menu__scrim cd-menu__scrim--visible';
+    }
+
+    toggle.addEventListener('click', function () {
+      if (slidingMenu.getAttribute('data-toggle') === 'open') {
+        closeDrawer();
+      } else {
+        openDrawer();
+      }
+    });
+
+    scrim.addEventListener('click', closeDrawer);
+
+    each(closeButtons, function (closeButton) {
+      closeButton.addEventListener('click', closeDrawer);
+    });
+
+    each(menuExpansions, function (menuExpansion) {
+      var slidingMenuSecondary = menuExpansion.parentElement.querySelector('.cd-menu__slidingMenuSecondary');
+      var backButton = slidingMenuSecondary.querySelector('.cd-menu__backButton');
+      if (backButton) {
+        backButton.addEventListener('click', function () {
+          slidingMenuSecondary.setAttribute('data-toggle', 'closed');
         });
       }
-
-      function openDrawer () {
-        slidingMenu.setAttribute('data-toggle', 'open');
-        scrim.className = 'cd-menu__scrim cd-menu__scrim--visible';
-      }
-
-      toggle.addEventListener('click', function () {
-        if (slidingMenu.getAttribute('data-toggle') === 'open') {
-          closeDrawer();
-        } else {
-          openDrawer();
-        }
-      });
-
-      scrim.addEventListener('click', closeDrawer);
-
-      each(closeButtons, function (closeButton) {
-        closeButton.addEventListener('click', closeDrawer);
-      });
-
-      each(menuExpansions, function (menuExpansion) {
-        var slidingMenuSecondary = menuExpansion.parentElement.querySelector('.cd-menu__slidingMenuSecondary');
-        var backButton = slidingMenuSecondary.querySelector('.cd-menu__backButton');
-        if (backButton) {
-          backButton.addEventListener('click', function () {
-            slidingMenuSecondary.setAttribute('data-toggle', 'closed');
-          });
-        }
-        if (menuExpansion) {
-          menuExpansion.addEventListener('click', function () {
-            slidingMenuSecondary.setAttribute('data-toggle', 'open');
-          });
-        }
-      });
-    })();
-
-    // Menu Dropdowns
-    (function () {
-      var profileDropdown = document.querySelector('.cd-menu__desktopNav .cd-menu__profile');
-      var dropdowns = document.querySelectorAll('.cd-menu__dropdown');
-
-      function closeDropdowns (e) {
-        if (e.target !== profileDropdown && e.target.parentElement !== profileDropdown) {
-          profileDropdown.setAttribute('data-toggle', 'closed');
-        }
-        each(dropdowns, function (dropdown) {
-          if (e.target !== dropdown && e.target.parentElement !== dropdown &&
-            e.target.parentElement.parentElement !== dropdown) {
-            dropdown.setAttribute('data-toggle', 'closed');
-          }
+      if (menuExpansion) {
+        menuExpansion.addEventListener('click', function () {
+          slidingMenuSecondary.setAttribute('data-toggle', 'open');
         });
       }
+    });
+  });
 
+  // Menu Dropdowns
+  window.addEventListener('load', function () {
+    var profileDropdown = document.querySelector('.cd-menu__desktopNav .cd-menu__profile');
+    var dropdowns = document.querySelectorAll('.cd-menu__dropdown');
+
+    function closeDropdowns (e) {
+      if (e.target !== profileDropdown && e.target.parentElement !== profileDropdown) {
+        profileDropdown.setAttribute('data-toggle', 'closed');
+      }
       each(dropdowns, function (dropdown) {
-        dropdown.addEventListener('click', function () {
-          if (this.getAttribute('data-toggle') === 'open') {
-            this.setAttribute('data-toggle', 'closed');
-          } else {
-            this.setAttribute('data-toggle', 'open');
-          }
-        });
-      });
-
-      document.getElementsByTagName('body')[0].addEventListener('click', closeDropdowns);
-
-      document.getElementsByTagName('body')[0].addEventListener('touchstart', closeDropdowns);
-    })();
-
-    // Profile Dropdown
-    (function () {
-      var menuLinks = [{
-        url: '/dashboard/my-dojos',
-        title: 'My Dojos'
-      }, {
-        url: '/dashboard/dojos/events/user-events',
-        title: 'My Events'
-      }];
-
-      var loginRegisters = document.querySelectorAll('.cd-menu__account');
-      var profiles = document.querySelectorAll('.cd-menu__profile');
-      var profileMenus = document.querySelectorAll('.cd-menu__profileMenu');
-      var profileNames = document.querySelectorAll('.cd-menu__profileName');
-      var profileLinks = document.querySelectorAll('.cd-menu__profileLink');
-      var profilePics = document.querySelectorAll('.cd-menu__profilePic');
-      var profileDropdown = document.querySelector('.cd-menu__desktopNav .cd-menu__profile');
-
-      function request (url, postData, success, error) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-          if (this.readyState === 4 && this.status === 200) {
-            try {
-              var responseJson = JSON.parse(this.responseText);
-              success(responseJson);
-            } catch (e) {
-              error(e);
-            }
-          } else if (this.readyState === 4) {
-            error(this);
-          }
-        };
-        xhr.open(postData ? 'POST' : 'GET', url, true);
-        xhr.withCredentials = true;
-        if (postData) {
-          xhr.setRequestHeader('Content-type', 'application/json');
+        if (e.target !== dropdown && e.target.parentElement !== dropdown &&
+          e.target.parentElement.parentElement !== dropdown) {
+          dropdown.setAttribute('data-toggle', 'closed');
         }
-        xhr.send(postData);
-      }
+      });
+    }
 
-      function showLoginRegister () {
-        each(loginRegisters, function (loginRegister) {
-          loginRegister.style.display = 'block';
-        });
-      }
-
-      function createMenuItem (title, url) {
-        var li = document.createElement('li');
-        var a = document.createElement('a');
-        a.href = url;
-        a.innerText = title;
-        li.appendChild(a);
-        return li;
-      }
-
-      profileDropdown.addEventListener('click', function () {
+    each(dropdowns, function (dropdown) {
+      dropdown.addEventListener('click', function () {
         if (this.getAttribute('data-toggle') === 'open') {
           this.setAttribute('data-toggle', 'closed');
         } else {
           this.setAttribute('data-toggle', 'open');
         }
       });
+    });
 
-      request(zenBase + '/api/2.0/users/instance', null, function (userData) {
-        if (userData && userData.user) {
-          each(profileNames, function (profileName) {
-            profileName.innerText = userData.user.name;
-          });
-          each(profileLinks, function (profileLink) {
-            profileLink.href = zenBase + '/profile/' + userData.user.id;
-          });
-          request(zenBase + '/api/2.0/profiles/user-profile-data', JSON.stringify({
-            query: {
-              userId: userData.user.id
-            }
-          }), function (profile) {
-            request(zenBase + '/api/2.0/profiles/' + profile.id + '/avatar', null, function (avatar) {
-              if (avatar && avatar.imageData && avatar.imageInfo && avatar.imageInfo.type) {
-                each(profilePics, function (profilePic) {
-                  profilePic.src = 'data:' + avatar.imageInfo.type + ';base64,' + avatar.imageData;
-                });
-                each(profileMenus, function (profileMenu) {
-                  menuLinks.forEach(function (menuLink) {
-                    profileMenu.appendChild(createMenuItem(menuLink.title, zenBase + menuLink.url));
-                  });
-                });
-                each(profiles, function (profile) {
-                  profile.style.display = 'block';
-                });
-              } else {
-                showLoginRegister();
-              }
-            }, showLoginRegister);
-          }, showLoginRegister);
-        } else {
-          showLoginRegister();
+    document.getElementsByTagName('body')[0].addEventListener('click', closeDropdowns);
+
+    document.getElementsByTagName('body')[0].addEventListener('touchstart', closeDropdowns);
+  });
+
+  // Profile Dropdown
+  window.addEventListener('load', function () {
+    var cdfAdminMenuLinks = document.querySelectorAll('.cd-menu__cdfAdminLink');
+    var loginRegisters = document.querySelectorAll('.cd-menu__account');
+    var profiles = document.querySelectorAll('.cd-menu__profile');
+    var profileNames = document.querySelectorAll('.cd-menu__profileName');
+    var profileLinks = document.querySelectorAll('.cd-menu__profileLink');
+    var profilePics = document.querySelectorAll('.cd-menu__profilePic');
+    var profileDropdown = document.querySelector('.cd-menu__desktopNav .cd-menu__profile');
+    var refererLinks = document.querySelectorAll('.cd-menu__refererLink');
+
+    function request (url, postData, success, error) {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+          try {
+            var responseJson = JSON.parse(this.responseText);
+            success(responseJson);
+          } catch (e) {
+            error(e);
+          }
+        } else if (this.readyState === 4) {
+          error(this);
         }
-      }, showLoginRegister);
-    })();
+      };
+      xhr.open(postData ? 'POST' : 'GET', url, true);
+      xhr.withCredentials = true;
+      if (postData) {
+        xhr.setRequestHeader('Content-type', 'application/json');
+      }
+      xhr.send(postData);
+    }
+
+    function showLoginRegister () {
+      each(loginRegisters, function (loginRegister) {
+        loginRegister.style.display = 'block';
+      });
+    }
+
+    profileDropdown.addEventListener('click', function () {
+      if (this.getAttribute('data-toggle') === 'open') {
+        this.setAttribute('data-toggle', 'closed');
+      } else {
+        this.setAttribute('data-toggle', 'open');
+      }
+    });
+
+    request(zenBase + '/api/2.0/users/instance', null, function (userData) {
+      if (userData && userData.user) {
+        each(profileNames, function (profileName) {
+          profileName.innerText = userData.user.name;
+        });
+        each(profileLinks, function (profileLink) {
+          profileLink.href = zenBase + '/profile/' + userData.user.id;
+        });
+        request(zenBase + '/api/2.0/profiles/user-profile-data', JSON.stringify({
+          query: {
+            userId: userData.user.id
+          }
+        }), function (profile) {
+          request(zenBase + '/api/2.0/profiles/' + profile.id + '/avatar', null, function (avatar) {
+            if (avatar && avatar.imageData && avatar.imageInfo && avatar.imageInfo.type) {
+              each(profilePics, function (profilePic) {
+                profilePic.src = 'data:' + avatar.imageInfo.type + ';base64,' + avatar.imageData;
+              });
+              if (userData.user.roles.indexOf('cdf-admin') !== -1) {
+                each(cdfAdminMenuLinks, function (menuLink) {
+                  menuLink.style.display = 'block';
+                });
+              }
+              each(profiles, function (profile) {
+                profile.style.display = 'block';
+              });
+              each(refererLinks, function (link) {
+                link.addEventListener('click', function (e) {
+                  e.preventDefault();
+                  window.location.href = this.href + '?referer=' + encodeURIComponent(window.location);
+                });
+              });
+            } else {
+              showLoginRegister();
+            }
+          }, showLoginRegister);
+        }, showLoginRegister);
+      } else {
+        showLoginRegister();
+      }
+    }, showLoginRegister);
   });
 })();
